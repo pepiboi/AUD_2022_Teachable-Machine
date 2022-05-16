3d4c53914851d204021b18ea95ef25faa0fc5687
pepiboi <peta.ivanovi12345@gmail.com>
2022-05-16 13:11:57 +0200

    Tamino hat Resize Images überarbeitet

--- RezizeImages/src/resizeimages/ResizeImages.java
+++ RezizeImages/src/resizeimages/ResizeImages.java
@@ -10,8 +10,9 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
+import java.util.Scanner;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import javax.imageio.ImageIO;
 
 /**
@@ -24,17 +25,37 @@ public class ResizeImages {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            String file = "test";//replace with imagepath
-            File imagePath = new File(file);
-            Image image = ImageIO.read(new File(file));
-            BufferedImage buffered = (BufferedImage) image;
-            ImageIO.write(resizeImage(buffered), "jpg", imagePath);
-        } catch (IOException ex) {
-            System.out.println("Error while resizing images");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Images must be named the same basename and numbered after it beginning with one increasing by one continually to the last number");
+        System.out.println("How many images you want to resize?");
+        int imageNumber = scanner.nextInt();
+        System.out.println("Paste path of saved images (C\\...)");
+        String imagePath = scanner.next();
+        System.out.println("Paste name of images");
+        String baseImageName = scanner.next();
+        
+        String[] imageNames = new String[imageNumber];
+        for(int i=0;i<imageNumber;i++){
+            imageNames[i] = baseImageName + (i+1);
         }
+        for (int i = 0; i < imageNumber; i++) {
+            try {
+                File aktImagePath = new File(imagePath + "\\" + imageNames[i] + ".jpg");
+                Image image = ImageIO.read(aktImagePath);
+                BufferedImage buffered = (BufferedImage) image;
+                if (buffered.getHeight() < 2000 || buffered.getWidth() < 2000 || buffered.getHeight() > buffered.getWidth() * 2 || buffered.getHeight() * 2 < buffered.getWidth()) {
+                    ImageIO.write(resizeImage(buffered), "jpg", aktImagePath);
+                    System.out.println("image " + imageNames[i] + " resized successfully");
+                } else {
+                    System.out.println("bad image scaling for image " + imageNames[i]);
+                }
+            } catch (IOException ex) {
+                System.out.println("Wrong path or wrong file format of the picture " + (i+1));
+            }
+        }
+        System.out.println("Resizing finished");
     }
-    
+
     public static BufferedImage resizeImage(BufferedImage originalImage) {
         BufferedImage resizedImage = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics2D = resizedImage.createGraphics();
